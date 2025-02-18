f2145fbefbdecc59c8041ad143c3c72222bb9766
Ellaira Torio <ellaira523@gmail.com>
2025-02-18 21:08:34 +1300

    updated view model

--- FantaLabels/MVVM/ViewModel/MainViewModel.cs
+++ FantaLabels/MVVM/ViewModel/MainViewModel.cs
@@ -6,6 +6,23 @@ namespace FantaLabels.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
-        public MainViewModel() { }
+
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel() 
+        {
+            HomeVM= new HomeViewModel();
+            CurrentView = HomeVM;
+        }
     }
 }
